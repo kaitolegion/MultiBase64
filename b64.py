bd48d60b9410452f2abd535d295667474240fc53
Kaito Coding <47966968+kaitolegion@users.noreply.github.com>
2021-02-02 18:17:26 +0800

    Update Multi Encode B64

--- b64.py
+++ b64.py
@@ -1,11 +1,12 @@
 from base64 import b64encode, b64decode
 
-flag = "fl4g{...}"
+flag = input("Enter Flag Format (flag{....}):")
+ts = int(input("How many times (10,20,30):"))
 flagencode = b64encode(flag.encode())
 flagencode1 = b64encode(flagencode).decode()
 output = str(flagencode1)
 n = 1
-while n != 20:
+while n != ts:
    fe = b64encode(output.encode())
    fe2= b64encode(fe).decode()
    output = output.replace(output,str(fe2))
